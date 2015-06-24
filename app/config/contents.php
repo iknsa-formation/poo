35904a93a194718a8ebd53bf73932e5556788f62
Khalid Sookia <khalidsookia@gmail.com>
2015-06-24 11:13:55 +0200

    Updated id in contents"

--- app/config/contents.php
+++ app/config/contents.php
@@ -2,18 +2,21 @@
 
 $_SERVER['contents'] = array(
     array(
+        'id' => "1",
         'auteur' => "Aurelie",
         "titre" => "Hello",
         "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. ",
         "created" => "2015-06-22 11:00:00"
     ),
     array(
+        "id" => "2",
         "auteur" => "Gilles",
         "titre" => "PHP",
         "description" => " Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum Dolorum voluptatem magnam corporis, quaerat eum",
         "created" => "2015-06-22 12:00:00"
     ),
     array(
+        "id" => "3",
         "auteur" => "Khalid",
         "titre" => "lorem",
         "description" => " accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe. accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe. accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe. accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe. accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe. accusamus ipsa deserunt totam aliquid pariatur hic mollitia doloremque omnis, veniam eius quia cumque unde saepe.",

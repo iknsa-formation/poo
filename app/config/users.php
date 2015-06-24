91185af2cde0b4f29ccd1d4353449417cc387353
Khalid Sookia <khalidsookia@gmail.com>
2015-06-24 11:12:46 +0200

    Added id to users

--- app/config/users.php
+++ app/config/users.php
@@ -2,18 +2,21 @@
 
 $_SERVER['users'] = [
     [
+        "id" => "1",
         "nom" => "Khalid",
         "email" => "khalidsookia@gmail.com",
         "salt" => "123456789",
         "password" => "f19e3760488202ab92a6ea356c2c9fe8"
     ],
     [
+        "id" => "2",
         "nom" => "Aurelie",
         "email" => "aurelie.birot@club.fr",
         "salt" => "azerty",
         "password" => "1e26ada73f2e1b5a31087357559a4c4f"
     ],
     [
+        "id" => "3",
         "nom" => "Gilles",
         "email" => "gilles.fourot@atos.net",
         "salt" => "wxcv",
